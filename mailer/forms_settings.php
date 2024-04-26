1b478c754b5c0f48ff81b56550a464ffdea24f05
Chakridhar2555 <nissankararaochakridhar@gmail.com>
2024-04-26 23:28:39 +0530

    first commit

---
+++ mailer/forms_settings.php
@@ -0,0 +1,8 @@
+<?php
+	/* Forms settings */
+	$subj = "New message from the site 'Onovo - Portfolio Agency Template'"; //letter subject
+	$to = 'info@bqinfotech.com'; // 
+	$from = 'teamapartx@bqinfotech.com'; // Admin e-mail
+	$fromName = 'Bqinfotech'; // Your company name
+	$charset = 'UTF-8';
+?>
